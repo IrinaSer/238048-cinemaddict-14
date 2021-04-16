f3b20d48af80929c73e5f29fb940b945536fc00a
irin <puhkaya@selectel.ru>
2021-04-16 13:19:19 +0300

    Revert "refactor"
    This reverts commit 6d0082e40e69a813669f05603016019cf28a582b.

--- src/view/extra-films.js
+++ src/view/extra-films.js
@@ -1,18 +1,7 @@
-import {createListMovieCardTemplate} from './list-movie-card.js';
-
 export const createExtraFilmsTemplate = (title) => {
-  const MOVIE_COUNT = 2;
-  const cards = [];
-
-  for (let i = 0; i < MOVIE_COUNT; i++) {
-    cards.push(createListMovieCardTemplate());
-  }
-
   return `<section class="films-list films-list--extra">
-      <h2 class="films-list__title">${title}</h2>
+    <h2 class="films-list__title">${title}</h2>
 
-      <div class="films-list__container">
-        ${cards.join('')}
-      </div>
+    <div class="films-list__container"></div>
   </section>`;
 };
