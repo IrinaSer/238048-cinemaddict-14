219a6894aae629f88a9a721c4345060b4b15607a
irin <puhkaya@selectel.ru>
2021-04-24 16:37:28 +0300

    refactor render method

--- src/utils.js
+++ src/utils.js
@@ -13,4 +13,31 @@ const getRandomFloat = (a = 1, b = 0) => {
   return lower + Math.random() * (upper - lower);
 };
 
-export {getRandomInteger, getRandomFloat};
+const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const RenderPosition = {
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+};
+
+const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export {getRandomInteger, getRandomFloat, renderTemplate, RenderPosition, renderElement, createElement};
